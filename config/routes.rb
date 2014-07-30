1b617d9a8310f5f54b9f5dada741d245016f90fc
k2-toyama <z12020kt@aiit.ac.jp>
2014-07-31 02:44:14 +0900

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+root 'static_pages#home'
+match '/help', to:'static_pages#help',  via:'get'
+match '/about', to:'static_pages#about', via:'get'
+match '/contact',to:'static_pages#contact',via:'get'
 end
